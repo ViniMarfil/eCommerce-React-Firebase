f83145399f791b3b595a8869e3d4cd7ee13336b5
ViniMarfil <65354928+ViniMarfil@users.noreply.github.com>
2023-01-15 21:27:44 -0300

    Added product fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,41 @@
+//Components
 import Header from "./components/Header";
-import useLocalStorage from "use-local-storage";
-import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import Footer from "./components/Footer";
 import ProductPage from "./pages/ProductPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { CartContextProvider } from "./contexts/CartContext";
-import { DUMMY_DATA } from "./data/DUMMY_DATA";
-import Footer from "./components/Footer";
-import { UserContextProvider } from "./contexts/UserContext";
 import AccountPage from "./pages/AccountPage";
+//Misc
+import { Routes, Route } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
+import { db } from "./api/firebase";
+import { collection, getDocs, query, limit } from "firebase/firestore";
+import { DUMMY_DATA } from "./data/DUMMY_DATA";
+//Contexts
+import { CartContextProvider } from "./contexts/CartContext";
+import { UserContextProvider } from "./contexts/UserContext";
+import { useState, useEffect } from "react";
 
 function App() {
   const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+  const [popularProducts, setPopularProducts] = useState([]);
+
   function switchTheme() {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   }
+
+  useEffect(() => {
+    //In truth, it only gets 15 products. Close enough.
+    const getPopularProducts = async () => {
+      const q = query(collection(db, "products"), limit(15));
+      const data = await getDocs(q);
+      setPopularProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPopularProducts();
+  }, []);
 
   return (
     <UserContextProvider>
@@ -29,7 +48,7 @@ function App() {
         >
           <Header theme={theme} switchTheme={switchTheme} />
           <Routes>
-            <Route path="/" element={<MainPage products={DUMMY_DATA} />} />
+            <Route path="/" element={<MainPage products={popularProducts} />} />
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/account" element={<AccountPage />} />
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
-import ProductSection from "../components/ProductSection";
+import ProductSection from "../components/Products/ProductSection";
+
 
 
 function MainPage({products}) {
