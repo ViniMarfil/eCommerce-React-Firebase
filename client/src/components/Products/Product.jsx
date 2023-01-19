f7abcec6dbaf7bf76ce9d5677089b3f6745d8849
ViniMarfil <65354928+ViniMarfil@users.noreply.github.com>
2023-01-19 14:07:21 -0300

    fixed the products to work in mobile

--- client/src/components/Products/Product.jsx
+++ client/src/components/Products/Product.jsx
@@ -4,11 +4,21 @@ import { FaRegHeart } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-const Product = ({ id, title, price, description, image, rating }) => {
+const Product = ({ id, title, price, description, image }) => {
   return (
-    <div className="relative mb-4 h-80 overflow-hidden rounded-lg border bg-white shadow-slate-900 hover:shadow-md dark:shadow-slate-100/20 ">
-      <div className="relative flex h-full w-full flex-col items-center justify-center">
-        <div className="group absolute flex h-full w-full flex-row items-center justify-center transition hover:bg-slate-600/40">
+    <div className="mb-4 h-80 min-w-[16rem] overflow-hidden rounded-lg border bg-white shadow-slate-900 hover:shadow-md dark:shadow-slate-100/20">
+      <div className="relative z-20 flex h-[80%] w-full flex-col items-center justify-center">
+        <img
+          className="mb-10 h-full bg-inherit object-contain transition hover:scale-105 "
+          src={image}
+          alt={title}
+        />
+        <div className="absolute bottom-0 text-orange-600 text-3xl font-semibold">
+          {"$"+price}
+        </div>
+      </div>
+      <div className="h-[20%] bg-slate-100 flex flex-row justify-center items-center ">
+        <div className="">
           <Link to={`/product/${id}`}>
             <ProductButton>
               <RiSearchLine />
@@ -22,12 +32,7 @@ const Product = ({ id, title, price, description, image, rating }) => {
             <FiShoppingCart />
           </ProductButton>
         </div>
-        <img
-          className="mb-4 h-[90%] transition hover:scale-105"
-          src={image}
-          alt={title}
-        />
-      </div>
+      </div>      
     </div>
   );
 };
@@ -35,7 +40,7 @@ const Product = ({ id, title, price, description, image, rating }) => {
 const ProductButton = ({ children, onClick }) => {
   return (
     <button
-      className="invisible m-2 rounded-full bg-slate-200 p-3 text-2xl text-slate-900 hover:bg-slate-300 hover:text-orange-500 hover:shadow-xl active:bg-slate-400 active:text-orange-600 group-hover:visible dark:bg-slate-400 dark:text-slate-200 dark:hover:bg-slate-500 dark:hover:text-orange-500 dark:hover:shadow-xl dark:active:bg-slate-600 dark:active:text-orange-600"
+      className="m-2 rounded-full border hover:border-orange-500 active:border-orange-600 border-slate-800/70 bg-slate-200 p-3 text-2xl text-slate-900 hover:bg-slate-300 hover:text-orange-500 hover:shadow-xl active:bg-slate-400 active:text-orange-600  dark:bg-slate-400 dark:text-slate-200 dark:hover:bg-slate-500 dark:hover:text-orange-500 dark:hover:shadow-xl dark:active:bg-slate-600 dark:active:text-orange-600"
       onClick={onClick}
     >
       {children}
--- client/src/components/Products/ProductSection.jsx
+++ client/src/components/Products/ProductSection.jsx
@@ -4,7 +4,7 @@ import Product from "./Product";
 const ProductSection = ({ products }) => {
   return (
     <section className="flex w-full flex-col items-center justify-center bg-slate-200 p-8 dark:bg-slate-700 dark:text-slate-200">
-      <h1 className="mb-8 text-5xl uppercase">popular Products</h1>
+      <h1 className="mb-8 text-5xl uppercase">Popular Products</h1>
 
       <div className="mx-auto grid max-w-sm grid-cols-1 gap-8 md:mx-0 md:max-w-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {products.map((product) => {
