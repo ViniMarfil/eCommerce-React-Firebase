b7733315a96cf86cb6b94dd371fab13e00e0f921
ViniMarfil <65354928+ViniMarfil@users.noreply.github.com>
2023-01-25 01:19:22 -0300

    Added add from wishlist button

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,24 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
+import WishlistContext from "../contexts/WishlistContext";
 
 function ProductPage() {
   const { id } = useParams();
   const { getProduct } = useContext(UserContext);
+  const { isProductInWishlist, addOrRemoveWishlistItem } =
+    useContext(WishlistContext);
 
   //Fetching product data
   const [product, setProduct] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function getProductThenSet() {
+    async function getProductDetails() {
       setIsLoading(true);
       const result = await getProduct(id);
       result.success ? setProduct(result.data) : setProduct(null);
-      console.log(result);
       setIsLoading(false);
     }
-    getProductThenSet();
+    getProductDetails();
   }, []);
 
   //Quantity logic
@@ -46,24 +48,24 @@ function ProductPage() {
             src={product.image}
             alt={product.title}
           />
-          <div className="flex flex-col items-center md:items-start md:px-20 ">
+          <div className="flex flex-col items-center mt-2 md:items-start md:px-20 ">
             <h1 className="mb-10 block items-center text-3xl">
               {product.title}
             </h1>
             <h3 className="mb-10  max-w-4xl text-sm">{product.description}</h3>
 
-            <div className="mb-10 text-7xl md:text-5xl text-orange-600">
+            <div className="mb-10 text-7xl text-orange-600 md:text-5xl">
               {"$" + product.price}
             </div>
-            <div className="md: flex flex-row justify-center">
-              <div className="mr-8 flex max-w-md select-none flex-row text-5xl">
+            <div className="flex-row justify-center md:flex">
+              <div className="mr-4 mb-2 flex max-w-md select-none flex-row items-center justify-center text-5xl md:mb-0">
                 <span
                   className="cursor-pointer "
                   onClick={() => changeQuantity(-1)}
                 >
                   -
                 </span>
-                <div className="mx-2 select-none rounded-lg border border-slate-800 px-4 text-right  dark:border-slate-200">
+                <div className="mx-2 select-none rounded-lg border border-slate-800 px-4 text-right dark:border-slate-200">
                   {quantity}
                 </div>
                 <span
@@ -73,8 +75,20 @@ function ProductPage() {
                   +
                 </span>
               </div>
-              <button className="whitespace-nowrap text-md cursor-pointer break-keep rounded bg-orange-600 px-8 py-3  text-white shadow hover:bg-orange-700 focus:outline-none active:bg-orange-500 ">
+              <button
+                className="text-md my-1 mr-4 w-full whitespace-nowrap break-keep rounded bg-orange-600 px-8 py-3 text-white
+               shadow hover:bg-orange-700 focus:outline-none active:bg-orange-500"
+              >
                 Add to cart
+              </button>
+              <button
+                className="text-md my-1 w-full whitespace-nowrap break-keep rounded bg-slate-100 px-8 py-3 font-medium text-orange-600 shadow
+               hover:bg-slate-200 hover:text-orange-700 focus:outline-none active:bg-slate-50 active:text-orange-500 md:w-auto"
+                onClick={() => addOrRemoveWishlistItem(id)}
+              >
+                {isProductInWishlist(id)
+                  ? "Remove from wishlist"
+                  : "Add to wishlist"}
               </button>
             </div>
           </div>
@@ -91,6 +105,7 @@ function ProductPage() {
     );
   }
 
+  //Not found
   return (
     <section className="flex min-h-[80vh] w-full flex-col items-center bg-slate-200 p-8 text-2xl dark:bg-slate-700 dark:text-slate-200">
       <h1>Error! Looks like the product you searched for vanished.</h1>
